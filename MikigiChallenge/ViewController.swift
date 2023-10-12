79143fcae752455d8d80257616c0d1ba3663c37f
Mustafa Deveci <m.deveci9@hotmail.com>
2023-10-12 17:22:19 +0300

    created project

---
+++ MikigiChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MikigiChallenge
+//
+//  Created by mustafa deveci on 12.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
